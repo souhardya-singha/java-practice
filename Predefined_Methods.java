efdf448f8cf0e14d9763642d390b507d0b1cecc9
souhardya-singha <singhasouhardya42@gmail.com>
2026-04-11 15:03:33 +0530

    syntax error sloved

--- Predefined_Methods.java
+++ Predefined_Methods.java
@@ -2,7 +2,7 @@ public class Predefined_Methods {
     public static void main(String[] args)
     {
         Predefined_Methods obj = new Predefined_Methods();
-        System.out.println(obj.hashCgitode()); //hashcode is a predefined method
+        System.out.println(obj.hashCode()); //hashcode is a predefined method
         // The 'C' in hashCode must be capitalized!
     }
 }
