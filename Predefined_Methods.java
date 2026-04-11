c6d7d9f97534e106330d9a7eb41f1e59349ea513
souhardya-singha <singhasouhardya42@gmail.com>
2026-04-11 15:01:24 +0530

    predifined method

---
+++ Predefined_Methods.java
@@ -0,0 +1,8 @@
+public class Predefined_Methods {
+    public static void main(String[] args)
+    {
+        Predefined_Methods obj = new Predefined_Methods();
+        System.out.println(obj.hashCgitode()); //hashcode is a predefined method
+        // The 'C' in hashCode must be capitalized!
+    }
+}
