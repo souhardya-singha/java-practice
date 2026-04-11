d182035fccd1dd2cf90ef6ac73793141188553fa
souhardya-singha <singhasouhardya42@gmail.com>
2026-04-11 15:28:18 +0530

    starting arrays

---
+++ Arrays.java
@@ -0,0 +1,9 @@
+public class Arrays {
+    public static void main(String[] args)
+    {
+        int[]a ={10, 20, 30, 40};
+        System.out.println(a.length);
+        System.out.println(a[1]);
+    } 
+    
+}
