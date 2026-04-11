33fe6074811659b0ab1d63eb32ed6ec42a0c30bf
souhardya-singha <singhasouhardya42@gmail.com>
2026-04-11 15:12:21 +0530

    static method!!

---
+++ Static_Method.java
@@ -0,0 +1,12 @@
+class Test{
+    static void print(){
+        System.out.println("ayo wassup!!");
+    }
+}
+
+public class Static_Method {
+     public static void main(String[] args) {
+        Test.print(); // Call static method directly
+    }
+    
+}
