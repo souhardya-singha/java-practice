5292f029a771f6f26555e558e130b54eb816ebf0
souhardya-singha <singhasouhardya42@gmail.com>
2026-04-11 13:52:57 +0530

    Add Methods.java with user defined method example

---
+++ Methods.java
@@ -0,0 +1,15 @@
+//class void Methods{
+//        System.out.println("this is a user defined methos");
+//}
+class Methods
+{
+    void method(){
+System.out.println("this is a user defined method");
+    }
+//}
+public static void main(String[] args)
+{
+    Methods obj= new Methods();
+    obj.method();
+}
+}
