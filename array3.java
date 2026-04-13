808979dac1b5046a0685c4fc4d07411b90397987
souhardya-singha <singhasouhardya42@gmail.com>
2026-04-13 22:18:57 +0530

    passing array through methods

---
+++ array3.java
@@ -0,0 +1,15 @@
+
+    class array3 {
+    public static void main(String args[])
+    {
+        int arr[] = m1();
+
+        for (int i = 0; i < arr.length; i++)
+            System.out.print(arr[i] + " ");
+    }
+
+    public static int[] m1()
+    {
+        return new int[] { 1, 2, 3 };
+    }
+}
